bd6e583923e1a66e099eddcd33c2c0078355786f
elliotxx <951376975@qq.com>
2022-03-06 15:53:00 +0800

    :sparkles: feat: add Template type and funcs

--- types.go
+++ types.go
@@ -1,10 +1,59 @@
 package blueprint
 
-import "errors"
+import (
+	"errors"
+)
 
 const TemplateConfigurationFilenameWithoutExt = "blueprint"
 
 var ErrNotTemplateConfiguration = errors.New("not a template configuration file")
+
+type Template struct {
+	variableMap   map[string]*Variable
+	variableSlice []*Variable
+}
+
+func NewTemplate(tc *TemplateConfiguration) (*Template, error) {
+	t := &Template{
+		variableMap:   map[string]*Variable{},
+		variableSlice: []*Variable{},
+	}
+
+	for k, v := range tc.Variables {
+		nv, err := NewVariable(k, v.Description, v.Default)
+		if err != nil {
+			return nil, err
+		}
+
+		t.variableMap[k] = nv
+		t.variableSlice = append(t.variableSlice, nv)
+	}
+
+	return t, nil
+}
+
+func (t *Template) GetVariable(varName string) (Variable, error) {
+	if v, ok := t.variableMap[varName]; ok {
+		return *v, nil
+	}
+
+	return Variable{}, errors.New("variable not found")
+}
+
+func (t *Template) Variables() ([]Variable, error) {
+	result := []Variable{}
+
+	for _, v := range t.variableSlice {
+		nv, err := NewVariable(v.Name, v.Description, v.Default)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, *nv)
+	}
+
+	return result, nil
+}
 
 type TemplateConfiguration struct {
 	Variables map[string]Variable `yaml:"variables" json:"variables"`
@@ -14,4 +63,24 @@ type Variable struct {
 	Name        string `yaml:"name" json:"name"`
 	Description string `yaml:"description" json:"description"`
 	Default     string `yaml:"default" json:"default"`
+}
+
+func NewVariable(name, description, defaultValue string) (*Variable, error) {
+	if name == "" {
+		return nil, errors.New("name is empty")
+	}
+
+	if description == "" {
+		return nil, errors.New("description is empty")
+	}
+
+	if defaultValue == "" {
+		return nil, errors.New("defaultValue is empty")
+	}
+
+	return &Variable{
+		Name:        name,
+		Description: description,
+		Default:     defaultValue,
+	}, nil
 }
---
+++ types_test.go
@@ -0,0 +1,244 @@
+package blueprint
+
+import (
+	"reflect"
+	"testing"
+)
+
+var TestVariableMap = map[string]*Variable{
+	"Env": {
+		Name:        "Env",
+		Description: "The env name. One of dev,test,stable,pre,sim,gray,prod.",
+		Default:     "dev",
+	},
+	"ClusterName": {
+		Name:        "ClusterName",
+		Description: "The Cluster Name.",
+		Default:     "kubernetes-dev",
+	},
+	"Image": {
+		Name:        "Image",
+		Description: "The Image Address.",
+		Default:     "gcr.io/google-samples/gb-frontend:v4",
+	},
+}
+
+var TestVariableSlice = []*Variable{
+	{
+		Name:        "Env",
+		Description: "The env name. One of dev,test,stable,pre,sim,gray,prod.",
+		Default:     "dev",
+	},
+	{
+		Name:        "ClusterName",
+		Description: "The Cluster Name.",
+		Default:     "kubernetes-dev",
+	},
+	{
+		Name:        "Image",
+		Description: "The Image Address.",
+		Default:     "gcr.io/google-samples/gb-frontend:v4",
+	},
+}
+
+func TestNewTemplate(t *testing.T) {
+	type args struct {
+		tc *TemplateConfiguration
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *Template
+		wantErr bool
+	}{
+		{
+			name: "t1",
+			args: args{
+				tc: TestTemplateConfiguration,
+			},
+			want: &Template{
+				variableMap:   TestVariableMap,
+				variableSlice: TestVariableSlice,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewTemplate(tt.args.tc)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewTemplate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewTemplate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTemplate_GetVariable(t *testing.T) {
+	type fields struct {
+		variableMap   map[string]*Variable
+		variableSlice []*Variable
+	}
+
+	type args struct {
+		varName string
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    Variable
+		wantErr bool
+	}{
+		{
+			name: "t1",
+			fields: fields{
+				variableMap:   TestVariableMap,
+				variableSlice: TestVariableSlice,
+			},
+			args: args{
+				varName: "Env",
+			},
+			want:    *TestVariableMap["Env"],
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := &Template{
+				variableMap:   tt.fields.variableMap,
+				variableSlice: tt.fields.variableSlice,
+			}
+			got, err := tr.GetVariable(tt.args.varName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Template.GetVariable() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Template.GetVariable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTemplate_Variables(t *testing.T) {
+	type fields struct {
+		variableMap   map[string]*Variable
+		variableSlice []*Variable
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []Variable
+		wantErr bool
+	}{
+		{
+			name: "t1",
+			fields: fields{
+				variableMap:   TestVariableMap,
+				variableSlice: TestVariableSlice,
+			},
+			want: []Variable{
+				*TestVariableMap["Env"],
+				*TestVariableMap["ClusterName"],
+				*TestVariableMap["Image"],
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := &Template{
+				variableMap:   tt.fields.variableMap,
+				variableSlice: tt.fields.variableSlice,
+			}
+			got, err := tr.Variables()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Template.Variables() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Template.Variables() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewVariable(t *testing.T) {
+	type args struct {
+		name         string
+		description  string
+		defaultValue string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *Variable
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				name:         "test-name",
+				description:  "test-description",
+				defaultValue: "test-default-value",
+			},
+			want: &Variable{
+				Name:        "test-name",
+				Description: "test-description",
+				Default:     "test-default-value",
+			},
+			wantErr: false,
+		},
+		{
+			name: "missing-name",
+			args: args{
+				description:  "test-description",
+				defaultValue: "test-default-value",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "missing-description",
+			args: args{
+				name:         "test-name",
+				defaultValue: "test-default-value",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "missing-default",
+			args: args{
+				name:        "test-name",
+				description: "test-description",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewVariable(tt.args.name, tt.args.description, tt.args.defaultValue)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewVariable() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewVariable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
